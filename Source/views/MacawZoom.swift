313d2d6e9d6340e6d5b21ab3ebf459a474edfacb
NM61269N <nicolas-externe.magnat@edf.fr>
2020-02-13 17:49:15 +0100

    feat: limit zoom and pan to node boundaries

--- Source/views/MacawZoom.swift
+++ Source/views/MacawZoom.swift
@@ -16,7 +16,7 @@ import AppKit
 
 open class MacawZoom {
 
-    private var view: MView!
+    private var view: MacawView!
     private var onChange: ((Transform) -> Void)!
     private var touches = [TouchData]()
     private var zoomData = ZoomData()
@@ -50,7 +50,7 @@ open class MacawZoom {
         onChange(zoomData.transform())
     }
 
-    func initialize(view: MView, onChange: @escaping ((Transform) -> Void)) {
+    func initialize(view: MacawView, onChange: @escaping ((Transform) -> Void)) {
         self.view = view
         self.onChange = onChange
     }
@@ -87,7 +87,7 @@ open class MacawZoom {
         let s1 = touches[0].point
         let e1 = touches[0].current(in: view)
         if touches.count == 1 {
-            return zoomData.move(delta: e1 - s1)
+            return zoomData.move(delta: e1 - s1, in: view)
         }
         let s2 = touches[1].point
         let e2 = touches[1].current(in: view)
@@ -101,9 +101,8 @@ open class MacawZoom {
             let h = e1.y - scale * (s1.x * sina + s1.y * cosa)
             offset = Size(w: w, h: h)
         }
-        return ZoomData(offset: offset, scale: scale, angle: a).combine(with: zoomData)
+        return ZoomData(offset: offset, scale: scale, angle: a).combine(with: zoomData, in: view)
     }
-
 }
 
 fileprivate class ZoomData {
@@ -111,6 +110,9 @@ fileprivate class ZoomData {
     let offset: Size
     let scale: Double
     let angle: Double
+
+    private let maxScale: Double = 8
+    private let minScale: Double = 1
 
     init(offset: Size = Size.zero, scale: Double = 1, angle: Double = 0) {
         self.offset = offset
@@ -122,18 +124,58 @@ fileprivate class ZoomData {
         return Transform.move(dx: offset.w, dy: offset.h).scale(sx: scale, sy: scale).rotate(angle: angle)
     }
 
-    func move(delta: Size) -> ZoomData {
-        return ZoomData(offset: offset + delta, scale: scale, angle: angle)
+    func move(delta: Size, in view: MacawView) -> ZoomData {
+        return ZoomData(offset: limitOffset(offset: offset + delta, in: view, with: scale), scale: scale, angle: angle)
     }
 
-    func combine(with: ZoomData) -> ZoomData {
+    func combine(with: ZoomData, in view: MacawView) -> ZoomData {
+        let s = scale * with.scale
+        let a = angle + with.angle
+
+        var newScale = s
+        if newScale > maxScale {
+            newScale = maxScale
+        } else if newScale < minScale {
+            newScale = minScale
+        }
+
         let sina = sin(angle)
         let cosa = cos(angle)
         let w = offset.w + scale * (cosa * with.offset.w - sina * with.offset.h)
         let h = offset.h + scale * (sina * with.offset.w + cosa * with.offset.h)
-        let s = scale * with.scale
-        let a = angle + with.angle
-        return ZoomData(offset: Size(w: w, h: h), scale: s, angle: a)
+
+        return ZoomData(offset: limitOffset(offset: Size(w: w, h: h), in: view, with: newScale), scale: newScale, angle: a)
+    }
+
+    func limitOffset(offset: Size, in view:MacawView, with scale: Double) -> Size {
+        let viewSize = view.bounds.size
+        let nodeSize = view.node.bounds?.size() ?? Size.zero
+
+        let maxX: Double = 0
+        let minX = Double(viewSize.width) - Double(viewSize.width) * scale
+        let nodeHeightNotScaled = nodeSize.h * Double(viewSize.width) / nodeSize.w
+        let nodeHeight = nodeHeightNotScaled * scale
+        let topLimit = (nodeHeight - Double(viewSize.height) * scale) / 2
+        let bottomLimit = ((nodeHeightNotScaled - Double(viewSize.height)) / 2) * scale - (nodeHeight - Double(viewSize.height))
+
+        let minY = min(topLimit, bottomLimit)
+        let maxY = max(topLimit, bottomLimit)
+
+        var newW = offset.w
+        var newH = offset.h
+
+        if newW < minX {
+            newW = minX
+        } else if newW > maxX {
+            newW = maxX
+        }
+
+        if newH < minY {
+            newH = minY
+        } else if newH > maxY {
+            newH = maxY
+        }
+        return Size(w: newW, h: newH)
     }
 
 }
